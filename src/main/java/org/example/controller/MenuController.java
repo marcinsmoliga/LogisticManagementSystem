80dbd9efd4a547e1d4a120d391bd1f5fbbb436a9
marcinsmoliga <50251884+marcinsmoliga@users.noreply.github.com>
2020-06-14 14:09:09 +0200

    created MenuController

---
+++ src/main/java/org/example/controller/MenuController.java
@@ -0,0 +1,52 @@
+package org.example.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
+
+public class MenuController {
+
+    @FXML
+    private AnchorPane rootAnchorPane;
+
+    @FXML
+    private HBox menuHBox;
+
+    @FXML
+    private AnchorPane usersAnchorPane;
+
+    @FXML
+    private AnchorPane freightMarketAnchorPane;
+
+    @FXML
+    private AnchorPane lorriesAnchorPane;
+
+    @FXML
+    private AnchorPane driversAnchorPane;
+
+    @FXML
+    private StackPane fabsContainer;
+
+    @FXML
+    void switchToBuses(MouseEvent event) {
+
+    }
+
+    @FXML
+    void switchToDrivers(MouseEvent event) {
+
+    }
+
+    @FXML
+    void switchToTickets(MouseEvent event) {
+
+    }
+
+    @FXML
+    void switchToUsers(MouseEvent event) {
+
+    }
+
+}
