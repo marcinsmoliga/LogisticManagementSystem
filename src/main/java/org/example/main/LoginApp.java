2fb915ef7928d4e22857dc1518fb48c053a8fdfa
marcinsmoliga <50251884+marcinsmoliga@users.noreply.github.com>
2020-06-14 13:59:46 +0200

    created main/LoginApp

---
+++ src/main/java/org/example/main/LoginApp.java
@@ -0,0 +1,24 @@
+package org.example.main;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class LoginApp extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        StackPane mainPane = FXMLLoader.load(getClass().getResource("/fxml/login.fxml"));
+        Scene scene = new Scene(mainPane);
+        stage.setScene(scene);
+        stage.setTitle("Transport Management System");
+        stage.show();
+
+    }
+}
